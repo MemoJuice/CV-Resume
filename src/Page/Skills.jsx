4fd46094d0cdd9aaee801aac2e9033b54054419e
unknown <wrpxdouble1135@gmail.com>
2026-01-17 23:16:51 +0700

    arrow donw

--- src/Page/Skills.jsx
+++ src/Page/Skills.jsx
@@ -9,11 +9,65 @@ function Skills() {
       <div>
         <div className=" bg-linear-to-r from-green-500 via-yellow-300 to-yellow-600 bg-clip-text text-transparent font-semibold text-2xl flex flex-wrap gap-5 text-center items-center justify-center">
           <div className="border-2 border-white px-5 py-2 w-fill">React</div>
-          <div className="border-2 border-white px-5 py-2 w-fill">JavaScript</div>
-          <div className="border-2 border-white px-5 py-2 w-fill">Tailwind CSS</div>
+          <div className="border-2 border-white px-5 py-2 w-fill">
+            JavaScript
+          </div>
+          <div className="border-2 border-white px-5 py-2 w-fill">
+            Tailwind CSS
+          </div>
           <div className="border-2 border-white px-5 py-2 w-fill">Node.js</div>
           <div className="border-2 border-white px-5 py-2 w-fill">MongoDB</div>
           <div className="border-2 border-white px-5 py-2 w-fill">Git</div>
+        </div>
+        <div className="mt-15 flex justify-center">
+          <svg
+            width="600"
+            height="500"
+            viewBox="0 0 200 280"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              {/* การตั้งค่าไล่เฉดสีจากส้มไปม่วงตามรูปต้นฉบับ */}
+              <linearGradient
+                id="pathGradient"
+                x1="100%"
+                y1="0%"
+                x2="0%"
+                y2="100%"
+              >
+                <stop offset="25%" stopColor="#d6b617" /> {/* สีส้มพีช */}
+                <stop offset="50%" stopColor="#1e9892" /> {/* สีชมพูม่วง */}
+                <stop offset="100%" stopColor="#9B7CFF" /> {/* สีม่วงฟ้า */}
+              </linearGradient>
+
+              {/* หัวลูกศร (Arrowhead) */}
+              <marker
+                id="arrow"
+                viewBox="0 0 10 10"
+                refX="5"
+                refY="5"
+                markerWidth="6"
+                markerHeight="6"
+                orient="auto-start-reverse"
+              >
+                <path d="M 0 0 L 10 7 L 0 10 z" fill="#8480ed" />
+              </marker>
+            </defs>
+
+            {/* เส้น Path หลัก */}
+            <path
+              d="M150 20
+         C190 60, 60 70, 80 110
+         C100 150, 160 160, 120 200
+         C100 220, 90 220, 80 240"
+              stroke="url(#pathGradient)"
+              strokeWidth="7"
+              strokeLinecap="round"
+              fill="none"
+              markerEnd="url(#arrow)"
+            />
+          </svg>
         </div>
       </div>
     </div>
